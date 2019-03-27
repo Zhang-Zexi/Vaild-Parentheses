1408617de0f147d9ec2c4859e5c787612c3f6214
zhangzx <zhangzx@allinfinance.com>
2019-03-27 16:00:49 +0800

    添加测试用例

--- src/Solution.java
+++ src/Solution.java
@@ -32,4 +32,9 @@ class Solution {
         }
         return stack.isEmpty();
     }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isValid("()[]{}"));
+        System.out.println(new Solution().isValid("([)}"));
+    }
 }
